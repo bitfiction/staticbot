6ebf40fcb0fffb294afda2b7798dc3bd408d7505
osevce <sevce@bitfiction.com>
2025-04-23 09:12:52 +0200

    feature: prepare website deployment tfvars

--- infrastructure/accounts/bitfiction/main.tf
+++ infrastructure/accounts/bitfiction/main.tf
@@ -1,12 +1,10 @@
-# infrastructure/environments/prod/main.tf
-
 module "websites" {
-  source = "../../cloud_aws/" # This points to the root infrastructure directory
+  source = "../../cloud_aws/root_modules/static_website_custom_domain"
 
-  account_name    = var.account_name
-  aws_account = var.aws_account
-  websites    = var.websites
-  common_tags = var.common_tags
+  account_name = var.account_name
+  aws_account  = var.aws_account
+  websites     = var.websites
+  common_tags  = var.common_tags
 }
 
 # Re-export the outputs from the root module
