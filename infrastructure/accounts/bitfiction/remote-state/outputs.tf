a92bcbbc3c20f76ff7d543a0279fc195e07cfe77
osevce <sevce@bitfiction.com>
2025-04-23 09:15:36 +0200

    fix: remove manual deployments

--- infrastructure/accounts/bitfiction/remote-state/outputs.tf
+++
@@ -1,9 +0,0 @@
-# infrastructure/remote-state/outputs.tf
-
-output "state_bucket" {
-  value = aws_s3_bucket.terraform_state.bucket
-}
-
-output "dynamodb_table" {
-  value = aws_dynamodb_table.terraform_locks.name
-}
--- infrastructure/accounts/staticbot-dev/remote-state/main.tf
+++
@@ -1,9 +0,0 @@
-module "remote_state" {
-  source = "../../../cloud_aws/modules/remote-state"
-
-  account_name             = var.account_name
-  terraform_role_arn       = var.terraform_role_arn
-  external_id              = null
-  dynamo_table_name_suffix = "terraform-locks"
-  s3_bucket_name_suffix    = "terraform-state"
-}
