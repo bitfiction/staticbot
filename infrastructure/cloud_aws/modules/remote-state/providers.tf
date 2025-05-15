09f11f68bbd18ebdd334b72df6f6349aa5576b5a
osevce <sevce@bitfiction.com>
2025-05-15 09:30:50 +0200

    feat: providers

---
+++ infrastructure/cloud_aws/modules/remote-state/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  assume_role {
+    role_arn = var.terraform_role_arn
+    external_id = var.external_id
+  }
+}
